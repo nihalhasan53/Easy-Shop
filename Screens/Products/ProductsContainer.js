79baabd95e33dabf27e9d85cfb064cdfbcc3f6f4
Mohammed Nihal <nihalhasan53@gmail.com>
2021-04-24 16:22:34 +0530

    First Commit

---
+++ Screens/Products/ProductsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text,View,Button} from 'react-native'
+
+
+const ProductsContainer =()=>
+{
+    return(
+        <View>
+            <Text>Products Container</Text>
+        </View>
+    )
+}
+
+export function ProductsContainer;
